4aa8629b49b099c87b452ff7d79fce90007e3cb9
m-motawea <magedmotawea@gmail.com>
2019-03-13 10:22:41 +0200

    add threaded parser

--- parsers/xlsx_parser.py
+++ parsers/xlsx_parser.py
@@ -1,6 +1,9 @@
 import xlrd
 from .base import Parser
 import os
+from threading import Thread
+from multiprocessing import Lock
+
 
 class XLSXParser(Parser):
     def __init__(self, tab_names=[], keys=[]):
@@ -47,3 +50,96 @@ class XLSXParser(Parser):
             return result
         else:
             return []
+
+
+
+class ThreadedXLSXParser(Parser):
+    def __init__(self, tab_names=[], keys=[], no_threads=1):
+        self.tab_names = tab_names
+        self.keys = keys
+        self.no_threads = no_threads
+        self.result_lock = Lock()
+        self.result = []
+        self.threads = {}
+        self.thread_state = {}
+
+
+    def parse(self, path):
+        if not os.path.exists(path):
+            raise Exception("file {} does not exist".format(path))
+
+        workbook = xlrd.open_workbook(path, on_demand=True)
+        if self.tab_names:
+            for name in self.tab_names:
+                sheet = workbook.sheet_by_name(name)
+                self._parse_sheet(sheet)
+        else:
+            worksheet = workbook.sheet_by_index(0)
+            self._parse_sheet(worksheet)
+        end = False
+        while not end:
+            end = True
+            for thread in self.threads.values():
+                print("waiting for thread: {}".format(thread.__str__()))
+                if thread.is_alive():
+                    end = False
+        return self.result
+
+
+    def _parse_sheet(self, sheet: xlrd.sheet.Sheet):
+        index_key_map = {}
+        for col in range(sheet.ncols):
+            key = sheet.cell_value(0, col)
+            if key in self.keys:
+                index_key_map[key] = col
+
+        add_rows = (sheet.nrows - 1) % self.no_threads
+        step = int((sheet.nrows - 1) / self.no_threads)
+        for i in range(1, sheet.nrows, step):
+            t = Thread(target=self._parse_target, kwargs={
+                "sheet": sheet,
+                "key_col": index_key_map,
+                "start_row": i,
+                "end_row": i + step,
+                "name": str(i)
+            }, daemon=True)
+            t.start()
+            self.threads[str(i)] = t
+        if add_rows:
+            t = Thread(target=self._parse_target, kwargs={
+                "sheet": sheet,
+                "key_col": index_key_map,
+                "start_row": i,
+                "end_row": i + add_rows,
+                "name": "add_rows"
+            }, daemon=True)
+            t.start()
+            self.threads["add"] = t
+
+
+    def _parse_target(self, sheet: xlrd.sheet.Sheet, key_col: dict, start_row: int, end_row: int, name: str):
+        '''
+        :param sheet: sheet to be parsed
+        :param key_col: map key_name to column order ex: {"ip": 1, "username": 2}
+        :param start_row: set to 1 for single thread
+        :param end_row: set to sheet.nrows for single thread
+        :return: Flag: Bool, columns_processes: int or error msg
+        '''
+        try:
+            thread_result = []
+            for row_no in range(start_row, end_row):
+                row_dict = {}
+                for key in key_col:
+                    row_dict[key] = sheet.cell_value(row_no, key_col[key])
+                thread_result.append(row_dict)
+
+            self.result_lock.acquire()
+            self.result += thread_result
+            self.result_lock.release()
+            self.thread_state[name] = True
+            return True, end_row - start_row
+        except Exception as e:
+            self.result_lock.acquire()
+            self.thread_state[name] = True
+            self.result_lock.release()
+            return False, e
